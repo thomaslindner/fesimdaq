bd0ecedf191d98c3feedf97c90456947d55d3488
Thomas Lindner <lindner@triumf.ca>
2016-12-01 13:55:18 -0800

    new example code with checks for non-UTF-8 strings

--- fesimdaq_v2.c
+++ fesimdaq_v2.c
@@ -164,19 +164,142 @@ void seq_callback(INT h, INT hseq, void *info)
 
 }
 
+
+int is_utf8(const char * string)
+{
+    if(!string)
+        return 0;
+
+    const unsigned char * bytes = (const unsigned char *)string;
+    while(*bytes)
+    {
+        if( (// ASCII
+             // use bytes[0] <= 0x7F to allow ASCII control characters
+                bytes[0] == 0x09 ||
+                bytes[0] == 0x0A ||
+                bytes[0] == 0x0D ||
+                (0x20 <= bytes[0] && bytes[0] <= 0x7E)
+            )
+        ) {
+            bytes += 1;
+            continue;
+        }
+
+        if( (// non-overlong 2-byte
+                (0xC2 <= bytes[0] && bytes[0] <= 0xDF) &&
+                (0x80 <= bytes[1] && bytes[1] <= 0xBF)
+            )
+        ) {
+            bytes += 2;
+            continue;
+        }
+
+        if( (// excluding overlongs
+                bytes[0] == 0xE0 &&
+                (0xA0 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
+            ) ||
+            (// straight 3-byte
+                ((0xE1 <= bytes[0] && bytes[0] <= 0xEC) ||
+                    bytes[0] == 0xEE ||
+                    bytes[0] == 0xEF) &&
+                (0x80 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
+            ) ||
+            (// excluding surrogates
+                bytes[0] == 0xED &&
+                (0x80 <= bytes[1] && bytes[1] <= 0x9F) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
+            )
+        ) {
+            bytes += 3;
+            continue;
+        }
+
+        if( (// planes 1-3
+                bytes[0] == 0xF0 &&
+                (0x90 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
+                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
+            ) ||
+            (// planes 4-15
+                (0xF1 <= bytes[0] && bytes[0] <= 0xF3) &&
+                (0x80 <= bytes[1] && bytes[1] <= 0xBF) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
+                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
+            ) ||
+            (// plane 16
+                bytes[0] == 0xF4 &&
+                (0x80 <= bytes[1] && bytes[1] <= 0x8F) &&
+                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
+                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
+            )
+        ) {
+            bytes += 4;
+            continue;
+        }
+
+        return 0;
+    }
+
+    return 1;
+}
+
+
+
 /*-- Frontend Init -------------------------------------------------*/
 INT frontend_init()
 {
 
+  HNDLE directoryKey, keyHandle;  
+  int status = db_find_key(hDB,0,"/Equipment/SIMDAQ",&directoryKey);
+  
+  char key[16];
+  key[0] = 0x45;
+  key[1] = 0x75;
+  key[2] = 0x72;
+  key[3] = 0x7;
+  key[4] = 0xe2;
+  key[5] = 0x82;
+  key[6] = 0xac;
+  key[7] = 0x00;
+  
+  printf("Key %s\n",key);
+  
+  char keyName[160];
+  sprintf(keyName,"/Equipment/SIMDAQ/%s",key);
+  printf("Finding %s\n",keyName);
+  
+  //  HNDLE keyHandle;
+  //status = db_find_key(hDB,0,"/Equipment/SIMDAQ",&directoryKey);
+  //status = db_find_key(hDB,0,"/Equipment/SIMDAQ",&directoryKey); // step 2
+  status = db_find_key(hDB,0,keyName,&keyHandle); // step 2
 
-  for(int i = 0; i < 400; i++){
+  
+  printf("findkey status %i\n",status);
+
+
+  
+  if(status == DB_NO_KEY){
+    printf("Key doesn't exist\n");
+  } else if(status == DB_SUCCESS){
+    status = db_delete_key(hDB,keyHandle,FALSE);    
+    printf("Key exists; deletion %i\n",status);   
+  }
+    
+  if(is_utf8(key))
+    printf("%s is valid utf-8\n", key);
+  else
+    printf("%s is invalid utf-8\n", key);
+
+  
+  printf("creating new key\n");
+  status = db_create_key(hDB,directoryKey,key,TID_INT);
+  printf("Create key status = %i\n",status);
+  
+
+  for(int i = 0; i < 10; i++){
     printf("open record %i\n",i);
-    /*    const char *simdaqsettings_str = 
-      "[.]\n\
-Descrip = STRING : [256]\n\
-Parameter AAA = INT : 0\n\
-Parameter BB2 = INT : 0\n\
-Parameter CC22 = INT : 0";*/
 
     char set_this_str[256];
     sprintf(set_this_str,"[.]\n\
